24391d38c1b773f28e6271ba706bcd5b06f1e2e4
tyomnayark <tyomnayark@gmail.com>
2025-01-08 17:47:25 +0500

    add CONSTANTS

--- core_network/src/commonMain/kotlin/com/example/core_network/NetworkClient.kt
+++ core_network/src/commonMain/kotlin/com/example/core_network/NetworkClient.kt
@@ -3,19 +3,22 @@ package com.example.core_network
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.ktor.http.KtorHttpEngine
 import com.apollographql.ktor.ktorClient
+import com.example.core_network.utils.Constants.BASE_URL
+import com.example.core_network.utils.Constants.CONNECT_TIMEOUT
+import com.example.core_network.utils.Constants.REQUEST_TIMEOUT
+import com.example.core_network.utils.Constants.SOCKET_TIMEOUT
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.*
 
-const val BASE_URL = "https://api.spacexdata.com/v3"
 
 fun createKtorClient(): HttpClient {
     return HttpClient {
         install(HttpTimeout) {
-            requestTimeoutMillis = 10000
-            connectTimeoutMillis = 5000
-            socketTimeoutMillis = 15000
+            requestTimeoutMillis = REQUEST_TIMEOUT
+            connectTimeoutMillis = CONNECT_TIMEOUT
+            socketTimeoutMillis = SOCKET_TIMEOUT
         }
         install(ContentNegotiation) {
             json()
---
+++ core_network/src/commonMain/kotlin/com/example/core_network/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.core_network.utils
+
+import kotlinx.serialization.Required
+
+object Constants {
+    const val BASE_URL = "https://api.spacexdata.com/v3"
+
+    const val REQUEST_TIMEOUT = 10000L
+    const val CONNECT_TIMEOUT = 5000L
+    const val SOCKET_TIMEOUT = 15000L
+}
