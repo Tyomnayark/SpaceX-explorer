85589d5ce26a4a59ebf3f1d60864d0221c836eb9
tyomnayark <tyomnayark@gmail.com>
2025-01-08 17:39:44 +0500

    add domain and data modules

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,16 +1,12 @@
-import org.jetbrains.compose.ComposePlugin.CommonComponentsDependencies.resources
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinCocoapods)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.kotlinCocoapods)
-    alias(libs.plugins.kotlinSerialization)
-
 }
 
 kotlin {
@@ -31,23 +27,18 @@ kotlin {
         version = "1.0"
         ios.deploymentTarget = "16.0"
         framework {
-            baseName = "ComposeApp"
+            baseName = "domain"
             isStatic = true
-            transitiveExport = true
         }
     }
 
     sourceSets {
-        
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
 
             implementation(libs.koin.android)
-        }
 
-        iosMain.dependencies {
-//            implementation(libs.decompose.extensions)
         }
 
         commonMain.dependencies {
@@ -65,51 +56,26 @@ kotlin {
             implementation(libs.koin.compose)
 
             implementation(libs.decompose)
-            implementation(libs.decompose.extensions)
+//            implementation(libs.decompose.extensions)
+        }
 
-
-            implementation(project(":features:main_screen"))
-            implementation(project(":features:settings"))
-
-            implementation(project(":core_network"))
-
-            implementation(project(":data:settings_data"))
-            implementation(project(":data:main_screen_data"))
-
-            implementation(project(":domain:settings_domain"))
-            implementation(project(":domain:main_screen_domain"))
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
         }
     }
 }
 
 android {
-    namespace = "org.example.project"
+    namespace = "com.example.domain"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "org.example.project"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-}
 
-dependencies {
-    debugImplementation(compose.uiTooling)
 }
-
